fa81fca4402820ad6a4b58ae8bc402fe4c9b6062
Fedorov German <99956255+N9Nys@users.noreply.github.com>
2024-02-16 10:26:16 +0300

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,63 @@
+export function getDateFormat(date) {
+  const dateParsed = date.slice(0, date.length - 3);
+  const time = new Date(dateParsed);
+
+  let today = dateParsed.slice(4, 6).replace(/[\s.,%]/g, '');
+  
+  let hours = time.getHours();
+  if (date.includes("PM")) {
+    hours = hours + 12;
+  } else {
+    if (parseInt(hours) < 10) {
+      hours = '0' + hours;
+    }
+  }
+
+  let minutes = time.getMinutes();
+  minutes = minutes< 10 ? ('0' + minutes) : minutes;
+
+  let month;
+
+  switch(dateParsed.slice(0, 3)) {
+    case "Jan":
+      month = "Января";
+      break;
+    case "Feb":
+      month = "Февраля";
+      break;
+    case "Mar":
+      month = "Марта";
+      break;
+    case "Apr":
+      month = "Апреля";
+      break;
+    case "May":
+      month = "Мая";
+      break;
+    case "Jun":
+      month = "Июня";
+      break;
+    case "Jul":
+      month = "Июля";
+      break;
+    case "Aug":
+      month = "Августа";
+      break;
+    case "Sep":
+      month = "Сентября";
+      break;
+    case "Oct":
+      month = "Октября";
+      break;
+    case "Nov":
+      month = "Ноября";
+      break;
+    case "Dec":
+      month = "Декабря";
+      break;
+    default:
+      return "";
+  }
+
+  return (today + " " + month + " " + time.getFullYear());
+}
